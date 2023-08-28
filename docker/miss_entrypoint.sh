30252c37e2a4a953c2a3c1d1cf6d88968587b490
José Rafael Rebêlo <joserafaelrebelo@gmail.com>
2023-08-28 06:59:41 -0300

    fix(miss_autonomy): update submodules

--- docker/miss_entrypoint.sh
+++ docker/miss_entrypoint.sh
@@ -6,7 +6,4 @@ colcon build --symlink-install
 
 source install/setup.bash
 
-# export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$ALBOT_WS/src/aws-robomaker-small-warehouse-world/models/:$ALBOT_WS/src/albot-description/models/
-# chmod 777 -R /${MISS_WS}/src
-
 exec "$@"
