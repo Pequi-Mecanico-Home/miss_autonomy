e41943503898b8a8db3bc544737e8440ae5b011d
José Rafael Rebêlo <joserafaelrebelo@gmail.com>
2023-08-30 03:08:09 -0300

    chore(miss_autonomy): update entrypoint

--- docker/miss_entrypoint.sh
+++ docker/miss_entrypoint.sh
@@ -6,4 +6,7 @@ colcon build --symlink-install
 
 source install/setup.bash
 
+#Script to copy model files do Gazebo Path so it can run third-party worlds
+source /miss_ws/src/miss_worlds/setup_models.sh
+
 exec "$@"
