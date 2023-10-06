fe7ec2d1405e4602001bdb37c7968fa3e3317cbc
José Rafael Rebêlo <joserafaelrebelo@gmail.com>
2023-10-06 08:57:40 -0300

    feat(miss_autonomy): updating setup for real robot

--- docker/miss_entrypoint.sh
+++ docker/miss_entrypoint.sh
@@ -6,7 +6,4 @@ colcon build --symlink-install
 
 source install/setup.bash
 
-#Script to copy model files do Gazebo Path so it can run third-party worlds
-source /miss_ws/src/miss_worlds/setup_models.sh
-
 exec "$@"
